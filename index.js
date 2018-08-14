e2e3d51eaf1da61adf69278180bd70f8e05e9aa6
August Eymann <august.eymann@gmail.com>
2018-08-14 02:19:52 -0700

    Quick Update
    Quick update before I sleep.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 //Requirements
-//const linereader = require('./Functions/linereader.js')
+// const linereader = require('./Functions/linereader.js') I really was to lazy to make it synchronous will do it one
 const readline = require('readline-sync')
 const userMethod = require('./Classes/user.js')
 const objects = require('./Classes/objects.js')
 
 
 //Main
-let name = readline.question('What is your name?\n')
-let user = new userMethod(name)
+let user = new userMethod(readline.question('What is your name?\n'))
 user.addInventory(objects['Rusty Knife'])
-console.log('Welcome to the adventure game' + user.name)
+console.log('Welcome to the adventure game ' + user.name)
