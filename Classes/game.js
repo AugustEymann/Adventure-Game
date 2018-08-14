b815b151d391fbc4401fc805434903848c223d85
August Eymann <august.eymann@gmail.com>
2018-08-14 10:49:51 -0700

    Game Class and Beggining
    I added a game class to minimize code in index file. Plan on adding more in the next update.

---
+++ Classes/game.js
@@ -0,0 +1,11 @@
+
+function game() {
+    //Instance Variables
+    this.information = require('../information.json')
+    this.starting = this.information.Towns[Math.floor(Math.random() * this.information.Towns.length)]
+
+    //Methods
+     
+}
+
+module.exports = game
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 //Requirements
 // const linereader = require('./Functions/linereader.js') I really was to lazy to make it synchronous will do it one
 const readline = require('readline-sync')
-const userMethod = require('./Classes/user.js')
-const objects = require('./Classes/objects.js')
+const userClass = require('./Classes/user')
+const gameClass = require('./Classes/game')
+const objects = require('./Classes/objects')
 
 
 //Main
-let user = new userMethod(readline.question('What is your name?\n'))
+let game = new gameClass();
+let user = new userClass(readline.question('Do you remember your name?\n'))
 user.addInventory(objects['Rusty Knife'])
 console.log('Welcome to the adventure game ' + user.name)
+console.log('You were born in ' + game.starting) 
